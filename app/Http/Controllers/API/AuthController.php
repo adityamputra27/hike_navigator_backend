0f063aefde3188f860fd01754e8b8d7d3ce62822
dittmptr27 <adityamuhamadputra@gmail.com>
2023-11-06 10:39:13 +0700

    fix username after register

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -49,7 +49,7 @@ class AuthController extends Controller
                 ]);
             } else {
                 $user = new User;
-                $user->username = 'user';
+                $user->username = 'user'.date('YmdHis');
                 $user->name = $request->name;
                 $user->email = $request->email;
                 $user->role = 'HIKER';
